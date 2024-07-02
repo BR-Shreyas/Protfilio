ad9e37fbd4ed09ec0b4120231814900190ee246c
Shreyas B R <105351061+BR-Shreyas@users.noreply.github.com>
2024-07-02 13:34:26 +0530

    script.js

--- index.js
+++ index.js
@@ -1,56 +1,7 @@
-// console.log("hello world!");
-
-// let cars=["maruthi","swift","mahindra","thar"];
-// let fruits=["apple","bannana","orange","kiwi"];
-// console.log(cars);
-// console.log(cars[1]);
-// console.log(cars.length);
-// cars.join("*");
-// console.log(cars);
-// cars.pop("maruthi");
-// console.log(cars);
-// cars.push("rangerover");
-// console.log(cars);
-// //cars.unshift("rangerover");
-// console.log(cars);
-// let vid=cars.concat(fruits);
-// console.log(vid);
-// //Array indexOf()
-// Array lastIndexOf()
-// Array includes()
-
-
-// See Also:
-// Basic Methods
-// Sort Methods
-// Iteration Methods	Array find()
-// Array findIndex()
-// Array findLast()
-// Array findLastIndex()
-
-// Array sort()
-// Array reverse()
-// Array toSorted()
-// Array toReversed()
-// Sorting Objects
-
-// See Also:
-// Basic Methods
-// Search Methods
-// Iteration Methods	
-// Numeric Sort
-// Numeric Sort
-// Random Sort
-// Math.min()
-// Math.max()
-// Home made Min()
-// Home made Max()
-
-// alert("hello");
-// console.dir(document.body.childNodes);
-
-// let rio=document.getElementById("header");
-// console.log(rio);
- 
-let headings=document.getElementsByClassName("newr");
-console.log(headings);  
+var typed = new Typed(".text", {
+    strings: ["front-end developer", "backend-developer", "web developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
